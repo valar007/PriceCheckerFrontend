99f517e8d3e060e757f21dc3b48ed1da75516da3
Ramu <coursera.hasped@slmail.me>
2020-09-24 17:29:00 +0530

    Updates

---
+++ precache-manifest.52ba643258959171cd95f54d21eb54a4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d71e4f4101f7582d3077895e82cc53be",
+    "url": "/..valar007.github.io/PriceCheckerFrontend/index.html"
+  },
+  {
+    "revision": "749e75bea292f575eae6",
+    "url": "/..valar007.github.io/PriceCheckerFrontend/static/css/main.ccce78ee.chunk.css"
+  },
+  {
+    "revision": "730256b055a688be01ea",
+    "url": "/..valar007.github.io/PriceCheckerFrontend/static/js/2.f29d6111.chunk.js"
+  },
+  {
+    "revision": "5fd3cb9b2d06ef54c99c97a53f72b1c0",
+    "url": "/..valar007.github.io/PriceCheckerFrontend/static/js/2.f29d6111.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "749e75bea292f575eae6",
+    "url": "/..valar007.github.io/PriceCheckerFrontend/static/js/main.e33999cc.chunk.js"
+  },
+  {
+    "revision": "35a9268d47e347dba208",
+    "url": "/..valar007.github.io/PriceCheckerFrontend/static/js/runtime-main.b1c8d1cd.js"
+  }
+]);
